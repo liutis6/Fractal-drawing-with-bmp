53bd45474be4e709cad8485e4c67045d875a1ba4
Liutauras <liutauras.stepanenko@gmail.com>
2025-02-16 22:00:34 +0200

    Add drawing with given width + efficiency graphing

--- README.md
+++ README.md
@@ -1,26 +1,40 @@
 # Description:
 Python program for creating an image of a recursive pattern using a BMP file.
+Two use cases:
+    1. a depth is given - the program resizes the image to fit the fractal.
+    2. a pixel width is given - fractal stop drawing as soon as it reaches the specified width.
 
 Lots of comments as this is all new to me.
 
 # Instructions (***subject to change***):
+* Implement function according to use case
 * Run "main.py"
 
 # Structure (***subject to change***):
 * main.py - the entire program
 * output.bmp - gotten result
+* images - images for showcasing in readme
 
-
-# Todo ***subject to change***: 
+# Todo (***subject to change***): 
 * BMP file printing template
 * Recursive algorithm for pattern
+* Dynamic file size resizing
+* Stopping program when at given width
 * Input error protection
 * Testing
 * Benchmarking
 
 ## Pattern to replicate (Minkowski Sausage):
-![pattern](pattern.png "pattern")
+![pattern](images/pattern.png "pattern")
+
+## Example output
+* depth = 6,  line_len = 3:
+![depth6](images/d6.png "depth6")
+
+* depth = 3, line_len = 3
+![depth3](images/d3.png "depth3")
+
 
 ## References:
-- Inspiration for the recurrsion function: https://github.com/spirometaxas/minkowski-sausage-cli
+- Inspiration for the recursion function: https://github.com/spirometaxas/minkowski-sausage-cli
 - Definition: https://en.wikipedia.org/wiki/Minkowski_sausage
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from matplotlib import pyplot as plt
+import gc
+import time
+
 class BMP:
 	def __init__(self, height, width):
 		self.height = height
@@ -42,15 +46,14 @@ class BMP:
 		# must have a total size that is divisible by 4.
 		# if it is not, extra padding bytes (0x00) must be added at the end of each row
 		self.row_padding = (4-(bytes_per_row%4))%4
-			# width*3 - each pixel is 3 bytes, thus a row has width*3 bytes
-			# (width*3 % 4) - finds remainder
-			# 4-(width*3 % 4) - how many bytes are needed as filler in each row
-			# 4-(width*3 % 4) %4 - in case no padding is needed
+			# bytes_per_row%4 - finds remainder bytes
+			# 4-(bytes_per_row % 4) - how many bytes are needed as filler in each row
+			# (4-(bytes_per_row%4))%4 - in case no padding is needed
 			
 		self.row_size = bytes_per_row + self.row_padding
 
 		image_size = height * self.row_size # height amount of rows
-		file_size = 62 + image_size # sum of BMP(14 bytes) and DIB (40 bytes) headers
+		file_size = 62 + image_size # sum of header and image bytes
 		self.bmp_header[2:6] = file_size.to_bytes(4, 'little') # converts file_size into a 4-byte little-endian representation
 			# 4 byte little endian meaning that when the number is converted into hex form (0x00000000),
 			# it is divided into 4 pairs and then arranged in the array in such a way that the LSB is added first and MSB is added last.
@@ -58,13 +61,13 @@ class BMP:
 
 		# generate black bmp sheet of pixels
 		self.pixel_data = bytearray([0]*image_size)
-		
+
 	# change one pixel
 	def set_pixel(self, x, y):
 		if 0 <= x < self.width and 0 <= y < self.height:
 			index = (self.height-1-y) * self.row_size + (x // 8) # find exact byte
 			bit = 7 - (x % 8) # find index of the bit inside of the byte
-			# Set the pixel color
+
 			self.pixel_data[index] |= (1 << bit)
 				# at the index where the byte is, insert (bitwise left shift) a 1 into the index bit (0-7)
 				# the use bitwise OR assign to apply it
@@ -82,12 +85,14 @@ class BMP:
 	# type - section in the fractal
 	def rec_minkowski(self, n, type):
 		self.count += 1
-		if (n<=0):
+		if (self.x>=self.width):
+			return
+		elif (n<=0): # when max depth is reached start drawing
 			match type:
 				case 'left':
 					#print('left')
 					self.horizontal_line(self.y, self.x, self.x-self.line_len)
-					self.x = self.x - self.line_len
+					self.x = self.x - self.line_len # save last position to use later
 				case 'right':
 					#print('right')
 					self.horizontal_line(self.y, self.x, self.x+self.line_len+1)
@@ -139,12 +144,18 @@ class BMP:
 					self.rec_minkowski(n-1, 'right')
 					self.rec_minkowski(n-1, 'down')
 	
-	def draw_minkowski(self, x, y, depth, line_len=3):
+	def draw_minkowski(self, depth, line_len=3): # minimum 3 because at 
 		self.line_len = line_len
-		self.x = x
-		self.y = y
-		self.rec_minkowski(depth, 'right')
-		print("Operations: ", self.count)
+		self.x = 0
+		self.y = self.height//2
+		self.rec_minkowski(depth, 'right') # first call to recursion function
+		return self.count
+
+	def calculate_depth(self):
+		for n in range(1, 8): # 7 max depth as a precaution as any depth >7 breaches 20k pixel dimensions
+			size = ((4**n)*line_len)
+			if size>self.width:
+				return n
 
 	def generate_image(self, f_name):
 		with open(f_name, "wb") as file: # wb - write in binary mode
@@ -154,10 +165,97 @@ class BMP:
 			file.write(self.pixel_data)
 		print("BMP file created")
 
+def run_depth(depths):
+	t = []
+	c = []
+	for d in depths:
+		size = ((4**d)*line_len)
+		start = time.perf_counter()
+		image = BMP(size, size)
+		c.append(image.draw_minkowski(d, line_len))
+		image.generate_image(f"outputD{d}.bmp")
+		end = time.perf_counter()
+		t.append(end-start)
+		image = None # clear to not hog memory
+		gc.collect()
+	return (c, t)
+
+def run_width(widths):
+	t = []
+	c = []
+	for w in widths:
+		start = time.perf_counter()
+		image = BMP(w, w)
+		depth = image.calculate_depth()
+		c.append(image.draw_minkowski(depth, line_len))  # Starting point and size
+		image.generate_image(f"outputW{w}.bmp")
+		end = time.perf_counter()
+		t.append(end-start)
+		image = None
+		gc.collect()
+	return (c, t)
+
 if __name__ == '__main__':
-	depth = 5
 	line_len = 3
-	size = ((4**depth)*line_len)
-	image = BMP(size, size)
-	image.draw_minkowski(0, size//2, depth, line_len)  # Starting point and size
-	image.generate_image("output.bmp")
+
+	fig, axes = plt.subplots(2, 2, figsize=(12,6)) # 1 line, 2 graphs
+	ax1, ax2, ax3, ax4 = axes.flatten()
+	fig.suptitle("Graphs of Minkowski sausage performance")
+
+	ds = [1, 3, 5, 7]
+
+	# depth x ops
+	cs, ts = run_depth(ds)
+	ax1.plot(ds, cs, '-o')
+	ax1.spines['right'].set_visible(False)
+	ax1.spines['top'].set_visible(False)
+	for i, num in enumerate(cs):
+		ax1.annotate(f'{num:.2e}', (ds[i], cs[i]), xytext=(-6,5), textcoords='offset points')
+	ax1.set_xlabel('Depth')
+	ax1.set_ylabel('Operations')
+	ax1.set_yscale('log')
+
+	# depth x time
+	ax3.plot(ds, ts, '-o')
+	ax3.spines['right'].set_visible(False)
+	ax3.spines['top'].set_visible(False)
+	for i, num in enumerate(ts):
+		ax3.annotate(f'{num:.2e}', (ds[i], ts[i]), xytext=(-10,5), textcoords='offset points')
+	ax3.set_xlabel('Depth')
+	ax3.set_ylabel('Time')
+	ax3.set_yscale('log')
+
+	ws = [5000, 10000, 15000, 20000]
+
+	# width x ops
+	cs, ts = run_width(ws)
+	ax2.plot(ws, cs, '-o')
+	ax2.spines['right'].set_visible(False)
+	ax2.spines['top'].set_visible(False)
+	for i, num in enumerate(cs):
+		ax2.annotate(f'{num:.2e}', (ws[i], cs[i]), xytext=(-10,5), textcoords='offset points')
+	ax2.set_xlabel('Width')
+	ax2.set_ylabel('Operations')
+	ax2.set_yscale('log')
+
+	# width x time
+	ax4.plot(ws, ts, '-o')
+	ax4.spines['right'].set_visible(False)
+	ax4.spines['top'].set_visible(False)
+	for i, num in enumerate(ts):
+		ax4.annotate(f'{num:.2e}', (ws[i], ts[i]), xytext=(-10,5), textcoords='offset points')
+	ax4.set_xlabel('Width')
+	ax4.set_ylabel('Time')
+	ax4.set_yscale('log')
+
+	plt.tight_layout()
+	plt.show()
+
+	# TODO
+	# make algoritm for making image with given width
+	# decide and setup test cases
+	# make 4 graphs
+		# 2 graphs - with given depth
+			# 1 shows time v depth, other 1 - operations v depth
+ 		# 2 graphs - with given width
+			# 1 shows time v width, other 1 - operations v width
